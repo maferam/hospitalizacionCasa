f4ea72632a6b351a420deda84b25a15fa061af4e
rodameni <rodemeni@gmail.com>
2022-09-26 14:09:14 -0500

    Se agrega HospitalRDM

--- authApp/models/medico.py
+++ authApp/models/medico.py
@@ -1,6 +1,6 @@
 from django.db import models
 from .user import User
-from.paciente import paciente
+
 
 class medico(models.Model):
     id_medico = models.AutoField(primary_key=True)
@@ -8,4 +8,3 @@ class medico(models.Model):
     nombre = models.CharField('nombre', max_length=45)
     correo= models.EmailField('Correo',max_length=45)
     rol= models.CharField('rol',max_length=45)
-    id_paciente= models.ForeignKey(paciente, related_name='acudiente', on_delete=models.CASCADE)
--- authApp/models/paciente.py
+++ authApp/models/paciente.py
@@ -1,5 +1,8 @@
 from django.db import models
+
+from authApp.models.medico import medico
 from .user import User
+from .medico import medico
 
 class paciente(models.Model):
     id_paciente = models.AutoField(primary_key=True)
@@ -7,3 +10,4 @@ class paciente(models.Model):
     nombre = models.CharField('nombre', max_length=45)
     fecha_nacimiento= models.DateField('fecha de nacimiento')
     direccion= models.CharField('dirección',max_length=45)
+    id_medico= models.ForeignKey(medico, related_name='medico', on_delete=models.CASCADE)
